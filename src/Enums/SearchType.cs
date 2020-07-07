d0c6bba3d7bb674c6cd50b9796af528b13e40dbb
Yucked <yucked@protonmail.com>
2020-07-07 19:59:29 -0400

    Reused enums

---
+++ src/Enums/SearchType.cs
@@ -0,0 +1,21 @@
+﻿namespace Victoria.Enums {
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum SearchType {
+        /// <summary>
+        /// 
+        /// </summary>
+        YouTube,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        SoundCloud,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Direct,
+    }
+}
