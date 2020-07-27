5acee79e25fad54d85df1f16a248a195bbb71435
Yucked <yucked@protonmail.com>
2020-07-26 21:58:11 -0400

    Added tests

---
+++ tests/SearchTests.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Victoria.Tests {
+    [TestClass]
+    public class SearchTests {
+        [DataTestMethod]
+        public async Task SearchYouTubeAsync() {
+            
+        }
+    }
+}
