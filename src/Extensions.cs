96f10eff5d3f3ce4063a50ec0fe7ede5242640fa
Yucked <yucked@protonmail.com>
2020-07-26 21:58:43 -0400

    Random and HttpClient singleton

---
+++ src/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Victoria {
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Extensions {
+        internal static readonly Random Random = new Random();
+        internal static readonly HttpClient HttpClient = new HttpClient();
+    }
+}
