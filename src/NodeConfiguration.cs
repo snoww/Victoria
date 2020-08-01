43c0badd72baaaf942f8adba59774d58ac500829
Yucked <yucked@protonmail.com>
2020-08-01 17:48:02 -0400

    Added NodeConfiguration.cs for AbstractLavaNode.cs

---
+++ src/NodeConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Victoria.Wrappers;
+
+namespace Victoria {
+    /// <summary>
+    /// 
+    /// </summary>
+    public class NodeConfiguration {
+        /// <summary>
+        /// 
+        /// </summary>
+        public DiscordClientWrapper DiscordClient { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Hostname { get; set; } = "127.0.0.1";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Port { get; set; } = 2333;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsSecure { get; set; } = false;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Authorization { get; set; } = "youshallnotpass";
+
+        internal string SocketEndpoint
+            => (IsSecure ? "wss" : "ws") + Endpoint;
+
+        internal string HttpEndpoint
+            => (IsSecure ? "https" : "http") + Endpoint;
+
+        internal string Endpoint
+            => $"://{Hostname}:{Port}/";
+    }
+}
