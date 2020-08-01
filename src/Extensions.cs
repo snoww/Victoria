b092f45e400789d76ff25ce3e8134690f0c9a645
Yucked <yucked@protonmail.com>
2020-08-01 17:48:14 -0400

    Added ReadAsJson{T} for HttpClient

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Victoria {
     /// <summary>
@@ -8,5 +10,40 @@ namespace Victoria {
     public static class Extensions {
         internal static readonly Random Random = new Random();
         internal static readonly HttpClient HttpClient = new HttpClient();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="httpClient"></param>
+        /// <param name="requestMessage"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="NullReferenceException"></exception>
+        /// <exception cref="HttpRequestException"></exception>
+        public static async Task<T> ReadAsJsonAsync<T>(this HttpClient httpClient, HttpRequestMessage requestMessage) {
+            if (httpClient == null) {
+                throw new ArgumentNullException(nameof(httpClient));
+            }
+
+            if (requestMessage == null) {
+                throw new ArgumentNullException(nameof(requestMessage));
+            }
+
+            if (requestMessage.RequestUri == null) {
+                throw new NullReferenceException(nameof(requestMessage.RequestUri));
+            }
+
+            using var responseMessage = await httpClient.SendAsync(requestMessage);
+            if (!responseMessage.IsSuccessStatusCode) {
+                throw new HttpRequestException(responseMessage.ReasonPhrase);
+            }
+
+            using var content = responseMessage.Content;
+            await using var stream = await content.ReadAsStreamAsync();
+
+            var deserialized = await JsonSerializer.DeserializeAsync<T>(stream);
+            return deserialized;
+        }
     }
 }
