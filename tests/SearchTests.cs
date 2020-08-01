8207c8612c4928d0c783903f5c5b79b0e88fafd2
Yucked <yucked@protonmail.com>
2020-08-01 17:50:45 -0400

    Tests that keep failing and failing me

--- tests/SearchTests.cs
+++ tests/SearchTests.cs
@@ -1,12 +1,25 @@
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Victoria.Interfaces;
+using Victoria.Responses.Search;
 
 namespace Victoria.Tests {
     [TestClass]
     public class SearchTests {
+        private readonly ILavaNode _lavaNode
+            = new AbstractLavaNode(new NodeConfiguration());
+
         [DataTestMethod]
-        public async Task SearchYouTubeAsync() {
+        [DataRow("The Weeknd Valerie")]
+        [DataRow("Logic OCD")]
+        public async Task SearchYouTubeAsync(string query) {
+            var searchResponse = await _lavaNode.SearchAsync(SearchType.YouTube, query);
             
+            Assert.IsNotNull(searchResponse);
+            Assert.IsNotNull(searchResponse.Exception, "searchResponse.Exception != null");
+            Assert.IsNotNull(searchResponse.Playlist, "searchResponse.Playlist != null");
+            Assert.IsNotNull(searchResponse.Tracks, "searchResponse.Tracks != null");
+            Assert.IsNotNull(searchResponse.Status, "searchResponse.Status != null");
         }
     }
 }
